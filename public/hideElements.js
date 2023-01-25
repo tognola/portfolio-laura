4f1f0efbf7050721cc35d9e9f73d3e7ca374f42f
Gaston Tognola <tognola08@gmail.com>
2023-01-25 17:38:55 +0100

    first commit

---
+++ public/hideElements.js
@@ -0,0 +1,28 @@
+function hideTransition( array ){
+
+    window.addEventListener("scroll", function () {
+        for (let i = 0; i < array.length; ++i) {
+            let card = array[i];
+            card.classList.add("transition-all")
+            let position = card.getBoundingClientRect();
+            if (position.bottom <= window.innerHeight) {
+               showElement(card)
+            }else {
+                hideElement(card)
+            }
+        }
+    });
+
+   
+}
+
+function hideElement(element){
+    element.classList.add("-translate-x-20");
+    element.classList.add("opacity-0");
+
+}
+function showElement(element){
+    element.classList.remove("-translate-x-20");
+    element.classList.remove("opacity-0");
+
+}
